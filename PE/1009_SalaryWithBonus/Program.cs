3b36f71e5bcbe3f52a90a6ee839fd42aaef4e083
Bruno Dias <brunoipb1@gmail.com>
2024-10-12 10:19:16 -0300

    Fist commit

---
+++ PE/1009_SalaryWithBonus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+Console.WriteLine("Digite seu nome:");
+string name = Console.ReadLine();
+Console.WriteLine("Digite seu sálario:");
+double salary = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite quanto você vendeu:");
+double sales = double.Parse(Console.ReadLine());
+
+double total = salary +sales*0.15;
+
+Console.WriteLine("TOTAL = R$ " + total.ToString("F2"));
+
+// Console.WriteLine(total.ToString("#.00"));

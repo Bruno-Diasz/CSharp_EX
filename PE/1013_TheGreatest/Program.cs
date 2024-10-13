4defa366062941800d3d0fb323cb3230ca6f25d1
Bruno Dias <brunoipb1@gmail.com>
2024-10-12 23:22:24 -0300

    O Maior

---
+++ PE/1013_TheGreatest/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] input = Console.ReadLine().Split(' ');
+int a = int.Parse(input[0]),b=int.Parse(input[1]), c=int.Parse(input[2]);
+int maior = (a+b+Math.Abs(a-b))/2;
+maior = (maior+c+Math.Abs(maior-c))/2;
+
+Console.WriteLine(maior + " eh o maior");

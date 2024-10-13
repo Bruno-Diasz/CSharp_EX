3cabfb23c9ba3fddc596599d1d7695d304ea529d
Bruno Dias <brunoipb1@gmail.com>
2024-10-13 15:22:20 -0300

    Distancia dois pontos

---
+++ PE/1015_DistanceTwoPoints/Program.cs
@@ -0,0 +1,6 @@
+﻿string [] inputP1 = Console.ReadLine().Split(' ');
+string [] inputP2 = Console.ReadLine().Split(' ');
+double[] p1= {double.Parse(inputP1[0]), double.Parse(inputP1[1])};
+double[] p2= {double.Parse(inputP2[0]), double.Parse(inputP2[1])};
+double distance = Math.Sqrt(Math.Pow(p2[0]-p1[0],2)+Math.Pow(p2[1]-p1[1],2));
+Console.WriteLine(distance.ToString("F4"));

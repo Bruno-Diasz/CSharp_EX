50509468eb048764fecbdd10fd92d570409cc7c1
Bruno Dias <brunoipb1@gmail.com>
2024-10-14 23:02:29 -0300

    Fibonacci Facil

---
+++ PE/1151_Easy_Fibonacci/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<int> numeros = new List<int>{0,1};
+
+for(int i= 1; i<n-1; i++)
+{
+    int numero =numeros[i]+numeros[i-1];
+    numeros.Add(numero);
+}
+
+Console.WriteLine(string.Join(" ",numeros));
+

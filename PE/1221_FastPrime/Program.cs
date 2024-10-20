bfa5af63cd950e0a1e85bfe0e2433b44ec523109
Bruno Dias <brunoipb1@gmail.com>
2024-10-20 14:52:51 -0300

    FastPrime

---
+++ PE/1221_FastPrime/Program.cs
@@ -0,0 +1,27 @@
+﻿int numeroEntradas = int.Parse(Console.ReadLine());
+for (int i = 0; i< numeroEntradas; i++)
+{
+    int numero = int.Parse(Console.ReadLine());
+    int div = 1;
+    bool prime =true;
+    for (int j = 1; j<numero; j++)
+    {
+        if (numero % j == 0)
+        {
+            div++;
+            if (div > 2){
+                prime = false;
+                break;
+            }
+            
+        }
+      
+
+    }
+    if (prime){
+        Console.WriteLine("Prime");
+    }else
+    {
+        Console.WriteLine("Not Prime");
+    }
+}
---
+++ PE/1247_CoastGuard/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] entrada;
+while ((entrada = Console.ReadLine().Split(' ')) != null)
+{
+    int distancia = int.Parse(entrada[0]);
+    int velocidadeFugitivo = int.Parse(entrada[1]);
+    int velocidadeGuarda = int.Parse(entrada[2]);
+    double limite = 12.0;
+    if ((limite/velocidadeFugitivo)<= (limite+distancia)/velocidadeGuarda)
+    {
+        Console.WriteLine("S");
+    }
+    else
+    {
+        Console.WriteLine("N");
+    } 
+}
